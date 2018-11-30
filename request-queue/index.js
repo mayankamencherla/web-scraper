a3cafd750ff8bc4c25f67027ded06144c23f022d
mayankamencherla <mayankamencherla24@gmail.com>
2018-11-30 03:38:14 +0000

    [crawler] Wrote request queue / sitemap classes to refactor logic from server.js

--- request-queue/index.js
+++ request-queue/index.js
@@ -1,24 +1,106 @@
+const axios   = require('axios');
+const cheerio = require('cheerio');
+const url     = require('url');
+
 /**
  * Maintains list of urls that have been crawled
  * and list of urls that need to be crawled
  */
 class RequestQueue {
-    constructor(sitemap) {
+    constructor(sitemap, base) {
         this.crawled = [];
         this.discovered = [];
         this.sitemap = sitemap;
+        this.base = base;
+    }
+
+    /**
+     * Return whether the link is valid
+     * TODO: Move to helpers
+     * @param link
+     */
+    validateUrl(link) {
+        if (link === undefined) return;
+
+        var parsed = url.parse(link);
+
+        return this.validateUrlFormat(parsed) &&
+               !this.alreadyCrawled(link) &&
+               !this.alreadyDiscovered(link);
+    }
+
+    /**
+     * Modify the url to absolute if relative
+     * TODO: Move to helpers
+     * @param url
+     * @return url
+     */
+    getAbsoluteUrl (url) {
+        if (url && url.length > 1 && url[0] == '/') {
+            url = this.base + url;
+        }
+
+        return url
+    }
+
+    /**
+     * Fetches the children of a parent url
+     * @param link
+     * @param callback
+     */
+    async fetchChildren(link) {
+
+        try {
+
+            const response = await axios.get(link);
+
+            console.log(`Fetching the children of ${link}`);
+
+            var $ = cheerio.load(response.data);
+
+            $('a').each((i, elem) => {
+                var url = this.getAbsoluteUrl(elem.attribs.href);
+
+                if (!this.validateUrl(url)) return;
+
+                this.discovered.push(url);
+
+                this.sitemap.addChild(link, url);
+            });
+        } catch (e) {
+            console.log(`Unable to fetch children for ${link}`);
+        }
     }
 
     /**
      * Crawls a link
      * @param link
      */
-    crawl(link) {
+    async crawlLink(link) {
         if (this.alreadyCrawled(link)) return;
 
         this.crawled.push(link);
 
         this.sitemap.addParent(link);
+
+        await this.fetchChildren(link);
+    }
+
+    /**
+     * Sets up the crawler job queue
+     */
+    async crawl() {
+        this.discovered.push(this.base);
+
+        while (this.discovered.length > 0) {
+            var url = this.discovered[0];
+
+            this.discovered.splice(0, 1);
+
+            await this.crawlLink(url);
+        }
+
+        console.log(`We are done with crawling the site: ${this.base}`);
     }
 
     /**
@@ -35,6 +117,25 @@ class RequestQueue {
      */
     alreadyDiscovered(link) {
         return this.discovered.includes(link);
+    }
+
+    /**
+     * Returns the base url's host name
+     */
+    getBaseHostName() {
+        var parsed = url.parse(this.base);
+
+        return parsed.hostname;
+    }
+
+    /**
+     * Returns whether the url format is valid
+     * @param parsed
+     */
+    validateUrlFormat(parsed) {
+        return parsed.protocol === 'https:' &&
+               parsed.hostname &&
+               parsed.hostname.includes(this.getBaseHostName());
     }
 }
 
--- server.js
+++ server.js
@@ -1,109 +1,18 @@
-const rp = require('request-promise');
 const express = require('express');
-const async = require('async');
-const _ = require('lodash');
-const url = require('url');
-const cheerio = require('cheerio');
-var request = require('request');
+const RequestQueue = require('./request-queue');
+const Sitemap = require('./sitemap');
 
-const base = 'https://www.monzo.com/';
+const base = 'https://www.monzo.com';
 
 const baseHostName = "monzo";
 
-var app = express();
+var app   = express();
+const map = new Sitemap();
+const rq  = new RequestQueue(map, base);
 
-var crawled = [];
-var inbound = [];
 
-// Url mapped to all its child links
-var adjacency = {};
-
-app.get('/', (req, res) => {
-
-    var makeRequest = (link, callback) => {
-        var startTime = new Date().getTime();
-
-        // Just simply
-        // if (Object.keys(adjacency).length >= 40) {
-        //     return;
-        // }
-
-        request(link, (error, response, body) => {
-            console.log(`Making a request to ${link}`);
-
-            var pageObject = {};
-
-            pageObject.links = [];
-
-            var $ = cheerio.load(body);
-
-            $('a').each((i, elem) => {
-              /*
-               * insert some further checks if a link is:
-               * valid
-               * relative or absolute
-               * check out the url module of node: https://nodejs.org/dist/latest-v5.x/docs/api/url.html
-               */
-              pageObject.links.push({linkText: $(elem).text(), linkUrl: elem.attribs.href})
-            });
-
-            callback(error, pageObject);
-        });
-    };
-
-    var loop = (link) => {
-        makeRequest(link, (err, pageObject) => {
-
-            // Create an adjacency matrix and print the graph using breadth first search
-
-            // console.log(`Callback with ${Object.keys(adjacency).length} keys`);
-
-            crawled.push(link);
-
-            adjacency[link] = [];
-
-            // Avoid making http and https requests to same link
-            var loopLinks = () => {
-                // console.log(`${crawled.length} links have been crawled and there are ${inbound.length} still remaining`);
-
-                if (inbound.length > 0) {
-                    var nextLink = inbound[0];
-
-                    inbound.splice(0, 1);
-
-                    loop(nextLink);
-                } else {
-                    console.log('Done!');
-                }
-            }
-
-            async.eachSeries(pageObject.links, (item, cb) => {
-
-                if (item && item.linkUrl && item.linkUrl !== undefined) {
-
-                    var parsedUrl = url.parse(item.linkUrl);
-
-                    if (parsedUrl.protocol === 'https:' &&
-                        parsedUrl.hostname &&
-                        parsedUrl.hostname.includes(baseHostName) &&
-                        !crawled.includes(item.linkUrl) &&
-                        !inbound.includes(item.linkUrl)) {
-
-                        // console.log(`Crawling... ${item.linkUrl}`);
-
-                        adjacency[link].push(item.linkUrl);
-
-                        inbound.push(item.linkUrl);
-                    }
-
-                    cb();
-                }
-            },
-            loopLinks);
-        });
-    };
-
-    loop(base);
+app.get('/', async (req, res) => {
+    await rq.crawl();
 });
 
 app.get('/print', (req, res) => {
